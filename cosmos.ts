0cbdad0ce26a4f99f5ec1c70bf208c6f029f214e
Dane Lipscombe <dane.lipscombe@planpay.com>
2024-05-04 18:31:19 +1000

    joystick

--- cosmos.ts
+++ cosmos.ts
@@ -12,7 +12,7 @@ const options: Options = {
   connector: "trrs",
   connectorSizeUSB: "average",
   connectorIndex: -1,
-  microcontroller: "kb2040-adafruit",
+  microcontroller: "waveshare-rp2040-zero",
   fastenMicrocontroller: true,
   verticalClearance: 0.1,
   clearScrews: true,
@@ -25,8 +25,8 @@ const options: Options = {
 // connectorIndex: 20.2
 
 const curvature = {
-  curvatureOfColumn: 15,
-  curvatureOfRow: 5,
+  curvatureOfColumn: 0,
+  curvatureOfRow: 0,
   spacingOfRows: 20.5,
   spacingOfColumns: 21.5,
   arc: 0
@@ -47,8 +47,8 @@ const pinkyCurvature = {
  * then translated by the z offset.
  */
 const upperKeysPlane = new Trsf()
-  .rotate(12, [0, 0, 0], [0, 1, 0], false)
-  .rotate(1, [0, 0, 0], [1, 0, 0], false)
+  .rotate(0, [0, 0, 0], [0, 1, 0], false)
+  .rotate(0, [0, 0, 0], [1, 0, 0], false)
   .translate(0, 0, 0, false)
 
 /** Definitions for the upper keys. */
@@ -61,13 +61,32 @@ const fingers: Key[] = [
     position: new Trsf()
       .placeOnMatrix({
         ...curvature,
-        column: 1,
-        row: 2
+        column: 0.5,
+        row: 1
       })
       .translate(0, 0, 0)
       .translate(-10, 0, 0)
       .transformBy(upperKeysPlane)
       .rotateTowards([0, 0, 1], 0.8)
+  },
+  {
+    type: "blank",
+    aspect: 1,
+    size: {
+      width: 55,
+      height: 10
+
+    },
+    cluster: "fingers",
+    position: new Trsf()
+      .placeOnMatrix({
+        ...curvature,
+        curvatureOfColumn: 0,
+        column: 0,
+        row: -1.5
+      })
+      .translate(0, 0, 0)
+      .transformBy(upperKeysPlane)
   },
   {
     type: "mx-better",
@@ -84,24 +103,6 @@ const fingers: Key[] = [
         ...curvature,
         column: -1,
         row: -0.5
-      })
-      .translate(0, 0, 0)
-      .transformBy(upperKeysPlane)
-  },
-  {
-    type: "mx-better",
-    keycap: {
-      profile: "xda",
-      row: 4,
-      letter: "m"
-    },
-    aspect: 1,
-    cluster: "fingers",
-    position: new Trsf()
-      .placeOnMatrix({
-        ...curvature,
-        column: -1,
-        row: 0.5
       })
       .translate(0, 0, 0)
       .transformBy(upperKeysPlane)
@@ -129,24 +130,6 @@ const fingers: Key[] = [
     type: "mx-better",
     keycap: {
       profile: "xda",
-      row: 4,
-      letter: ","
-    },
-    aspect: 1,
-    cluster: "fingers",
-    position: new Trsf()
-      .placeOnMatrix({
-        ...curvature,
-        column: 0,
-        row: 0.5
-      })
-      .translate(0, 2.8, -4)
-      .transformBy(upperKeysPlane)
-  },
-  {
-    type: "mx-better",
-    keycap: {
-      profile: "xda",
       row: 3,
       home: "ring",
       letter: "l"
@@ -158,24 +141,6 @@ const fingers: Key[] = [
         ...curvature,
         column: 1,
         row: -0.5
-      })
-      .translate(0, 0, 0)
-      .transformBy(upperKeysPlane)
-  },
-  {
-    type: "mx-better",
-    keycap: {
-      profile: "xda",
-      row: 4,
-      letter: "."
-    },
-    aspect: 1,
-    cluster: "fingers",
-    position: new Trsf()
-      .placeOnMatrix({
-        ...curvature,
-        column: 1,
-        row: 0.5
       })
       .translate(0, 0, 0)
       .transformBy(upperKeysPlane)
@@ -199,24 +164,6 @@ const fingers: Key[] = [
       .translate(0, -13, 6)
       .transformBy(upperKeysPlane)
   },
-  {
-    type: "mx-better",
-    keycap: {
-      profile: "xda",
-      row: 4,
-      letter: "/"
-    },
-    aspect: 1,
-    cluster: "fingers",
-    position: new Trsf()
-      .placeOnMatrix({
-        ...pinkyCurvature,
-        column: 2,
-        row: 0.5
-      })
-      .translate(0, -13, 6)
-      .transformBy(upperKeysPlane)
-  }
 ]
 
 /**
@@ -225,10 +172,10 @@ const fingers: Key[] = [
  * then offset by some amount.
  */
 const thumbOrigin = new Trsf()
-  .rotate(-11.890346750358203, [0, 0, 0], [1, 0, 0])
-  .rotate(-24.800771474656102, [0, 0, 0], [0, 1, 0])
-  .rotate(34.450218909733955, [0, 0, 0], [0, 0, 1])
-  .translate(-35.78333333333333, -25.666666666666668, -7.083333333333333)
+  .rotate(0, [0, 0, 0], [1, 0, 0])
+  .rotate(-70, [0, 0, 0], [0, 1, 0])
+  .rotate(0, [0, 0, 0], [0, 0, 1])
+  .translate(-25, 0, 0)
   .translateBy(new Trsf()
     .placeOnMatrix({
       ...curvature,
@@ -239,7 +186,7 @@ const thumbOrigin = new Trsf()
     .translate(8.75, -8.75, 0)
   )
   .rotate(0, [0, 0, 0], [0, 0, 1])
-  .translate(4, -5, 4)
+  .translate(0, 0, 0)
 
 /** The curvature of the thumb cluster. */
 const thumbCurvature = {
@@ -261,15 +208,15 @@ const thumbs: Key[] = [
     cluster: "thumbs",
     aspect: 1,
     position: new Trsf()
-      .rotate(17.8, [0, 0, 0], [1, 0, 0])
-      .rotate(3.3, [0, 0, 0], [0, 1, 0])
-      .rotate(-8.2, [0, 0, 0], [0, 0, 1])
+      .rotate(0, [0, 0, 0], [1, 0, 0])
+      .rotate(0, [0, 0, 0], [0, 1, 0])
+      .rotate(0, [0, 0, 0], [0, 0, 1])
       .placeOnMatrix({
         ...thumbCurvature,
-        column: 0.4,
-        row: -0.34
+        column: 0,
+        row: 0
       })
-      .translate(0, 0, 3.4)
+      .translate(0, 0, 0)
       .transformBy(thumbOrigin)
   },
   {
@@ -278,15 +225,15 @@ const thumbs: Key[] = [
     cluster: "thumbs",
     aspect: 1,
     position: new Trsf()
-      .rotate(15.1, [0, 0, 0], [1, 0, 0])
-      .rotate(16, [0, 0, 0], [0, 1, 0])
-      .rotate(-21.8, [0, 0, 0], [0, 0, 1])
+      .rotate(0, [0, 0, 0], [1, 0, 0])
+      .rotate(0, [0, 0, 0], [0, 1, 0])
+      .rotate(0, [0, 0, 0], [0, 0, 1])
       .placeOnMatrix({
         ...thumbCurvature,
-        column: 1.43,
-        row: -0.06
+        column: 0,
+        row: 1
       })
-      .translate(0, 0, 0.3)
+      .translate(0, 0, 0)
       .transformBy(thumbOrigin)
   }
 ]
